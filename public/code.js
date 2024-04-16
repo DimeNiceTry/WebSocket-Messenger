fa0acfbf941ed3a7fd40d61320d8d7e7a467a587
DimeNiceTry <chdd04@gmail.com>
2024-04-16 20:59:33 +0300

    LoginForm & RegForm added

--- public/code.js
+++ public/code.js
@@ -1,26 +1,58 @@
 (function(){
-    const app = document.querySelector(".app");
-    const socket = io();
+    const app = document.querySelector(".app"); //достали элемент с классом "app"
+    const socket = io(); // инициировали метод io, обозначенный в server.js
 
     let uname;
-
-    app.querySelector(".join-screen #join-user").addEventListener("click", function(){
+    /* обработка событий */
+    app.querySelector(".join-screen #username").addEventListener('keypress', function(e){ // На форме регистрации при нажатии Enter
+        if (e.key === 'Enter'){
+            let username = app.querySelector(".join-screen #username").value;
+            if(username.length === 0){
+                return;
+            }
+            socket.emit("newuser", username); // вызов метода io с параметрами
+            uname = username;
+            app.querySelector(".join-screen").classList.remove("active");
+            app.querySelector(".chat-screen").classList.add("active"); 
+        }
+    });
+    app.querySelector(".join-screen #join-user").addEventListener("click", function(){ // На форме регистрации при нажатии button
         let username = app.querySelector(".join-screen #username").value;
-        if(username.lenght === 0){
+        if(username.length  === 0){
             return;
         }
+        console.log(username);
         socket.emit("newuser", username);
         uname = username;
         app.querySelector(".join-screen").classList.remove("active");
         app.querySelector(".chat-screen").classList.add("active");
     });
-    
-    app.querySelector(".chat-screen #send-message").addEventListener('click', function(){
+
+    app.querySelector(".chat-screen #message-input").addEventListener('keypress', function(e){ // На форме мессенджера при нажатии Enter
+        if (e.key === 'Enter'){
+            let message = app.querySelector(".chat-screen #message-input").value;
+            if (message.length ===0){
+                return;
+            }
+            renderMessage("my", {
+                username:uname,
+                text:message
+            });
+            socket.emit("chat", {
+                username:uname,
+                text: message
+            });
+            app.querySelector(".chat-screen #message-input").value = "";
+        }
+        
+    });
+
+    app.querySelector(".chat-screen #send-message").addEventListener('click', function(){ // На форме мессенджера при нажатии Send
         let message = app.querySelector(".chat-screen #message-input").value;
-        if (message.lenght ===0){
+        if (message.length == 0){
             return;
         }
-        renderMessage("my", {
+        renderMessage("my", { //вызов функции renderMessage,описанной ниже
             username:uname,
             text:message
         });
@@ -32,7 +64,7 @@
     });
 
 
-    app.querySelector(".chat-screen #exit-chat").addEventListener("click", function(){
+    app.querySelector(".chat-screen #exit-chat").addEventListener("click", function(){ // На форме мессенджера при нажатии Exit
         socket.emit("exituser", uname);
         window.location.href = window.location.href;
     });
@@ -60,7 +92,7 @@
         }
         else if (type === "other"){
             let el = document.createElement("div");
-            el.setAttribute("class","message my-message");
+            el.setAttribute("class","message other-message");
             el.innerHTML = `
             <div>
                 <div class="name">${message.username}</div>
@@ -78,4 +110,47 @@
         messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
     }
 
+    //опеределение форм
+    let regForm = app.querySelector('#enterRegistration');
+    let withoutForm = app.querySelector('#enterWithoutLog');
+    let logForm = app.querySelector('#enterLogin');
+    //определение ссылок
+    let regLink = app.querySelector('#registration');
+    let logLink = app.querySelector('#login');
+    let backRegLink = app.querySelector('#backReg');
+    let backLogLink = app.querySelector('#backLog')
+
+    //Ссылки регистрации
+
+    regLink.addEventListener('mouseover', function(){
+        regLink.style.cursor = "pointer";
+    });
+    backRegLink.addEventListener('mouseover',function(){
+        backRegLink.style.cursor = "pointer";
+    });
+    regLink.addEventListener('click', function(){
+        withoutForm.classList.add('unactive');
+        regForm.classList.remove('unactive');
+    });
+    backRegLink.addEventListener('click', function(){
+        withoutForm.classList.remove('unactive');
+        regForm.classList.add('unactive');
+    });
+
+    //Ссылки логина
+
+    logLink.addEventListener('mouseover', function(){
+        logLink.style.cursor = "pointer";
+    });
+    backLogLink.addEventListener('mouseover',function(){
+        backLogLink.style.cursor = "pointer";
+    });
+    logLink.addEventListener('click', function(){
+        withoutForm.classList.add('unactive');
+        logForm.classList.remove('unactive');
+    });
+    backLogLink.addEventListener('click', function(){
+        withoutForm.classList.remove('unactive');
+        logForm.classList.add('unactive');
+    });
 })();
--- server.js
+++ server.js
@@ -1,25 +1,31 @@
+/* Запуск сервера и импортирование нужных библиотек */
 const express = require("express"); // Импортируем фреймвор Express
 const path = require("path"); //Импорт path для работы с файловой системой
 
 const app = express(); //вызов express, создаёт экземпляр приложения
 const server = require("http").createServer(app);   // создание http сервера, используя экземпляр app   
 
-const io = require("socket.io")(server);    
-
+const io = require("socket.io")(server);    // импорт socket.io
+const lox = require("socket.io")(server);
 app.use(express.static(path.join(__dirname+"/public")));
 /* express.static - обработка статических файлов из директории, указанной в аргументе
 Path.join - метод создание пути до директории. В него входит dirname - переменная, содержащая по умолчанию абсолютный путь к директории 
 и public, указывающий на директорию public. */
-io.on("connection", function(socket){
-    socket.on("newuser", function(username){
-        socket.broadcast.emit("update", username+" Joined the conversation");
+lox.on('connection', function(){
+    
+});
+io.on("connection", function(socket){ //метод "Прослушивания" события подключения к серверу. Всего в приложении 3 таких события: подключение или нового пользователя и отправка сообщений
+
+    socket.on("newuser", function(username){ //событие нового пользователя
+        socket.broadcast.emit("update", username+" Joined the conversation"); 
     })
-    socket.on("exituser", function(username){
+    socket.on("exituser", function(username){ //событие выхода пользователя
         socket.broadcast.emit("update", username+" Left the conversation");
     })
-    socket.on("chat", function(message){
+    socket.on("chat", function(message){ //событие написания сообщения в чат
         socket.broadcast.emit("chat", message);
     })
+    //broadcast.emit - отправка сообщения всем пользователям находящимся в сессии
 });
 
 server.listen(5000); //запуск сервера на порту 5000.   
