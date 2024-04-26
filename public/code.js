b183b2380dcc947d64bd770c8af21bf20171f506
DimeNiceTry <chdd04@gmail.com>
2024-04-26 13:16:47 +0300

    Released app 1.0

--- public/code.js
+++ public/code.js
@@ -4,29 +4,26 @@
 
     let uname;
     /* обработка событий */
-    app.querySelector(".join-screen #username").addEventListener('keypress', function(e){ // На форме регистрации при нажатии Enter
-        if (e.key === 'Enter'){
-            let username = app.querySelector(".join-screen #username").value;
-            if(username.length === 0){
-                return;
-            }
-            socket.emit("newuser", username); // вызов метода io с параметрами
-            uname = username;
+
+    document.addEventListener('DOMContentLoaded', function(){
+        savedUname = localStorage.getItem('activeUsername')
+        if (savedUname){
+            uname = savedUname;
+            socket.emit('showMessages', uname);
             app.querySelector(".join-screen").classList.remove("active");
-            app.querySelector(".chat-screen").classList.add("active"); 
+            app.querySelector(".chat-screen").classList.add("active");
+            app.querySelector('#uname').textContent = uname;
         }
-    });
-    app.querySelector(".join-screen #join-user").addEventListener("click", function(){ // На форме регистрации при нажатии button
-        let username = app.querySelector(".join-screen #username").value;
-        if(username.length  === 0){
-            return;
+        else{
+            app.querySelector(".join-screen").classList.add("active");
+            app.querySelector(".chat-screen").classList.remove("active");
         }
-        console.log(username);
-        socket.emit("newuser", username);
-        uname = username;
-        app.querySelector(".join-screen").classList.remove("active");
-        app.querySelector(".chat-screen").classList.add("active");
+        
     });
+
+
+
+
 
     app.querySelector(".chat-screen #message-input").addEventListener('keypress', function(e){ // На форме мессенджера при нажатии Enter
         if (e.key === 'Enter'){
@@ -43,6 +40,7 @@
                 text: message
             });
             app.querySelector(".chat-screen #message-input").value = "";
+            
         }
         
     });
@@ -65,20 +63,57 @@
 
 
     app.querySelector(".chat-screen #exit-chat").addEventListener("click", function(){ // На форме мессенджера при нажатии Exit
+        localStorage.removeItem('activeUsername');
         socket.emit("exituser", uname);
+        socket.emit('addMessage', {value: `${uname} Left the conversation`, uname: uname, type: 'enter'});
         window.location.href = window.location.href;
     });
 
     socket.on("update", function(update){
         renderMessage("update", update);
-    })
-
+        
+    });
     socket.on("chat", function(message){
         renderMessage("other", message);
-    })
+    });
 
-    function renderMessage(type,message){
+    socket.on('printMyMess', function(data){
+        const {value, uname, type} = data;
+        
+        renderMessage('preMy', data)
+    });
+    socket.on('printOtherMess', function(data){
+        renderMessage('preOther', data)
+    });
+    socket.on('printUpdateMess', function(data){
+        const {value, uname, type} = data;
+        renderMessage('update', data.message)
+    });
+
+    function renderMessage(type, message){
         let messageContainer = app.querySelector(".chat-screen .messages");
+        if (type === 'preMy'){
+            let el = document.createElement("div");
+            el.setAttribute("class","message my-message");
+            el.innerHTML = `
+            <div>
+                <div class="name">You</div>
+                <div class="text">${message.message}</div>
+            </div>
+            `;
+            messageContainer.appendChild(el);
+        }
+        if (type === 'preOther'){
+            let el = document.createElement("div");
+            el.setAttribute("class","message other-message");
+            el.innerHTML = `
+            <div>
+                <div class="name">${message.userfrom}</div>
+                <div class="text">${message.message}</div>
+            </div>
+            `;
+            messageContainer.appendChild(el);
+        }
         if (type ==="my"){
             let el = document.createElement("div");
             el.setAttribute("class","message my-message");
@@ -88,6 +123,8 @@
                 <div class="text">${message.text}</div>
             </div>
             `;
+            console.log(message);
+            socket.emit('addMessage', {value: message.text, uname: message.username, type: 'messages'})
             messageContainer.appendChild(el);
         }
         else if (type === "other"){
@@ -99,6 +136,8 @@
                 <div class="text">${message.text}</div>
             </div>
             `;
+            socket.emit('addMessage', {value: message.text, uname: message.username, type: 'messages'})
+
             messageContainer.appendChild(el);
         }
         else if (type ==="update"){
@@ -106,10 +145,11 @@
             el.setAttribute("class","update");
             el.innerHTML = message;
             messageContainer.appendChild(el);
+            
         }
         messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
     }
-
+console
     //опеределение форм
     let regForm = app.querySelector('#enterRegistration');
     let withoutForm = app.querySelector('#enterWithoutLog');
@@ -128,6 +168,8 @@
 
     let regButton = app.querySelector('#reg-user');
     let logButton = app.querySelector('#log-user');
+    let incorrectflag = app.querySelector('#incorrect');
+    let incorrectflagReg = app.querySelector('#incorrectReg');
     //Ссылки регистрации
 
     regLink.addEventListener('mouseover', function(){
@@ -162,24 +204,68 @@
         withoutForm.classList.remove('unactive');
         logForm.classList.add('unactive');
     });
-    
+
     regButton.addEventListener('click', function(){
-        socket.emit('addUser', {username: app.querySelector('#usernameReg').value, password:app.querySelector('#passwordReg').value});
+    
+        const username = app.querySelector('#usernameReg').value
+        const password = app.querySelector('#passwordReg').value
+        if (username.length === 0 || password.length === 0){
+            incorrectflagReg.textContent = 'Заполните поля!';
+            return;
+        }
+        else{
+            socket.emit('addUser', {username: username, password: password});
+        }
+       
+    })
+    socket.on('successReg', function(data){
+        const username = data;
+        localStorage.setItem(`activeUsername`, username);
+        
+        socket.emit('addMessage', {value: `${username} Joined the conversation`, uname: username, type: 'enter'});
+        socket.emit('showMessages', username);
+        app.querySelector('#uname').textContent = username;
         if(username.length  === 0){
             return;
         }
-        
+        socket.emit("newuser", username);
         uname = app.querySelector('#usernameReg').value;
         app.querySelector(".join-screen").classList.remove("active");
         app.querySelector(".chat-screen").classList.add("active");
     })
-    logButton.addEventListener('click', function(){
-        socket.emit('logUser', {username: app.querySelector('#usernameLog').value, password:app.querySelector('#passwordLog').value});
-        console.log(socket.emit('logUser', {username: app.querySelector('#usernameLog').value, password:app.querySelector('#passwordLog').value}));
+    socket.on('declinedReg', function(data){
+        incorrectflagReg.textContent = 'Такой логин уже существует!';  
+    })
+    socket.on('success', function(data){
+
+        const username = data;
+        socket.emit('addMessage', {value: `${username} Joined the conversation`, uname: username, type: 'enter'});
+        localStorage.setItem(`activeUsername`, username);
+        socket.emit('showMessages', username);
+        app.querySelector('#uname').textContent = username;
+        socket.emit("newuser", username);
+        
         if(username.length  === 0){
             return;
         }
 
+        uname = username;
+        app.querySelector(".join-screen").classList.remove("active");
+        app.querySelector(".chat-screen").classList.add("active");
+
+    });
+    socket.on('decline', function(data){
+        incorrectflag.textContent = 'Неправильный логин или пароль';        
+    });
+    logButton.addEventListener('click', function(){
+        const username = app.querySelector('#usernameLog').value;
+        const password = app.querySelector('#passwordLog').value;
+        if (username.length === 0 || password.length === 0){
+            incorrectflag.textContent = 'Заполните поля!';
+        }
+        else{
+            socket.emit('logUser', {username: app.querySelector('#usernameLog').value, password:app.querySelector('#passwordLog').value});
+        }
     })
 
 
